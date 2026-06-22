b19b9cc90a3a940f9ca3523d15d7b9d28edb860d
Xtract01 <meena.balwir149@gmail.com>
2026-06-23 02:02:58 +0530

    Added ratings to hotel

---
+++ src/db/migrations/20260622202631-add-ratings-to-hotel-table.ts
@@ -0,0 +1,20 @@
+"use strict";
+import { QueryInterface } from "sequelize";
+
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE hotels
+      ADD COLUMN rating DECIMAL(3,2) DEFAULT 0,
+      ADD COLUMN rating_count INT DEFAULT NULL;
+    `);
+  },
+
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE hotels
+      DROP COLUMN rating,
+      DROP COLUMN rating_count;
+    `);
+  },
+};
